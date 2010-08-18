c4c61c77d23e76ddd85fdcbdea68b70cd7862bf2
hidden <zzdhidden@gmail.com>
2010-08-18 10:53:01 +0800

    Desc

--- src/intro.js
+++ src/intro.js
@@ -2,10 +2,10 @@
  * Webim v@VERSION
  * http://www.webim20.cn/
  *
- * Copyright (c) 2009 Hidden
+ * Copyright (c) 2010 Hidden
  *
  * Date: 
- * Revision: 
+ * Commit: 
  */
 (function(window, document, undefined){
 
